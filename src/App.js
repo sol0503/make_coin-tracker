2868539b352e6eb4041fbfab8b36ac3bdaa7ff80
leesol <jbkljo@naver.com>
2022-08-26 15:46:15 +0900

    feat: 값변환 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoding] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState("");
+  const [price, setPrice] = useState("");
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const onSubmit = () => {};
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -15,17 +21,37 @@ function App() {
   return (
     <div>
       <h1>The Coins!{loading ? "" : `(${coins.length})`} </h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
+      <div>
+        <input
+          value={value}
+          placeholder="금액을 입력하시오"
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select>
+            {coins.map((coin, rank) => (
+              <option id={rank}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+      <div>
+        <button onClick={onSubmit}>입력하기</button>
+      </div>
+      <hr />
+      <div>
+        {coins.map((coin) => (
+          <option>
+            {coin.name}으로 환전한 값은? ${value * coin.quotes.USD.price}
+          </option>
+        ))}
+      </div>
     </div>
   );
 }
